12a8894bdd6c80eec00d2f4911ca68afb6943083
Mahmoud Ayman <mahmoud_aymann@outlook.com>
2022-11-07 17:24:35 +0200

    init

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.movie
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.movie.ui.theme.MovieComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,16 +26,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting()
+            Greeting {
+                Toast.makeText(this, "hiiii", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
 
 @Composable
-fun Greeting() {
-    Column(modifier = Modifier
-        .verticalScroll(rememberScrollState())
-        .fillMaxSize()) {
+fun Greeting(callBack: () -> Unit = {}) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+    ) {
         Image(
             painter = painterResource(id = R.drawable.burger_image),
             contentDescription = "burger image",
@@ -39,15 +47,58 @@ fun Greeting() {
                 .height(100.dp)
                 .fillMaxWidth()
         )
-        Column(modifier = Modifier.padding(16.dp).fillMaxWidth()) {
-            Text(text = "Meat berger", style = TextStyle(fontWeight = FontWeight.Bold))
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Meat berger", fontSize = 20.sp)
+                Text(
+                    text = "20$",
+                    style = TextStyle(color = Color.Blue),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
             Spacer(modifier = Modifier.padding(top = 10.dp))
             Text(text = "2 Pieces of meat")
             Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(text = "20$")
+            Button(
+                onClick = { callBack.invoke() },
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+            ) {
+                Text(text = "Order Now")
+            }
         }
     }
 }
+
+
+/*Column(
+    modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .fillMaxSize()
+) {
+    Image(
+        painter = painterResource(id = R.drawable.burger_image),
+        contentDescription = "burger image",
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+    )
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()) {
+        Text(text = "Meat berger", style = TextStyle(fontWeight = FontWeight.Bold))
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+        Text(text = "2 Pieces of meat")
+        Spacer(modifier = Modifier.padding(top = 10.dp))
+        Text(text = "20$")
+    }
+}}*/
 
 @Preview(showBackground = true)
 @Composable
